d7e9640948fee61c7da6ea7065b72be99cd977d2
Courtney Campany <courtneycampany@gmail.com>
2016-11-18 14:12:18 +1100

    added nurserys, setview doesnt include outof frame points

--- memap/global.R
+++ memap/global.R
@@ -12,17 +12,17 @@ locations_usa <- locations[1:6,]
 ##once click on nursery, bring up plots and generate climate data (possible populated them
 ## with sliders so they have user input)
 
-
-ncsulgo <- makeIcon(
-  iconUrl = "data/ncsu.png",
-  iconWidth = 35, iconHeight = 35
-)
-
-worklogos <- iconList(
-ncsu = makeIcon(iconUrl = "data/ncsu.png",iconWidth = 35, iconHeight = 35),
-app = makeIcon(iconUrl = "data/appstate.png",iconWidth = 35, iconHeight = 35),
-utk = makeIcon(iconUrl = "data/utk.png",iconWidth = 35, iconHeight = 35),
-ornl = makeIcon(iconUrl = "data/ornl.png",iconWidth = 35, iconHeight = 35),
-reno = makeIcon(iconUrl = "data/unr.png",iconWidth = 35, iconHeight = 35),
-idaho = makeIcon(iconUrl = "data/uidaho.jpg",iconWidth = 35, iconHeight = 35)
-)
+# 
+# ncsulgo <- makeIcon(
+#   iconUrl = "data/ncsu.png",
+#   iconWidth = 35, iconHeight = 35
+# )
+# 
+# worklogos <- iconList(
+# ncsu = makeIcon(iconUrl = "data/ncsu.png",iconWidth = 35, iconHeight = 35),
+# app = makeIcon(iconUrl = "data/appstate.png",iconWidth = 35, iconHeight = 35),
+# utk = makeIcon(iconUrl = "data/utk.png",iconWidth = 35, iconHeight = 35),
+# ornl = makeIcon(iconUrl = "data/ornl.png",iconWidth = 35, iconHeight = 35),
+# reno = makeIcon(iconUrl = "data/unr.png",iconWidth = 35, iconHeight = 35),
+# idaho = makeIcon(iconUrl = "data/uidaho.jpg",iconWidth = 35, iconHeight = 35)
+# )
--- memap/server.R
+++ memap/server.R
@@ -4,14 +4,17 @@ shinyServer(function(input, output) {
 
   output$plot <- renderLeaflet({
     
-    leaflet() %>% addTiles%>%
-      addCircleMarkers(locations_usa[1:nrow(locations_usa),3], 
-                 locations_usa[1:nrow(locations_usa),2],
-                 popup = locations_usa[1:nrow(locations_usa),1],
-                 col=c("red", "gold", "orange", "forestgreen", "navy", "black"),
-                 opacity=c(.85, 100, .85, .85, .85, .85),
-                fillColor = c("white", "black", "white", "white", "grey", "darkgoldenrod"),
-                 fillOpacity = c(100, 100, 100, 0, 85, 100), weight=3)
+    leaflet()  %>% addTiles%>%
+      addCircleMarkers(locations[1:nrow(locations),3], 
+          locations[1:nrow(locations),2],
+          popup = locations[1:nrow(locations),1],
+          col=c("red", "gold", "orange", "forestgreen", "navy", "black", "darkgreen", rep("darkgreen",19),"#990033"),
+          opacity=c(.85, 100, .85, .85, .85, .85, rep(.85, 21)),
+          fillColor = c("white", "black", "white", "white", "grey", "darkgoldenrod", "plum", 
+                        "white", rep("white",19)),
+          fillOpacity = c(100, 100, 100, 0, 85, 100, rep(100, 21)), weight=3)
+    
+  
     
   }) 
   
